c01c0f1bc7e6eed22a426bf3f35aaebc28f92a18
naveenknarsupalli <naveennk1421.gmail.com>
2021-11-23 17:27:17 +0530

    added relationship type configuration form

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import ConceptClassPage from "./pages/ConceptClassPage";
 import ConceptPage from "./pages/ConceptPage";
 import DrugPage from "./pages/DrugPage";
 import NavBar from "./pages/NavBar";
+import ModifyRealationship from "./components/relationship/ModifyRelationship";
+import RelationshipPage from "./pages/RelationshipPage";
 
 function App() {
   return (
@@ -27,6 +29,12 @@ function App() {
 
         <Route path="/drug/:id" component={() => <ModifyDrug />} />
         <Route path="/drug" component={() => <DrugPage />} />
+
+        <Route
+          path="/relationship/:id"
+          component={() => <ModifyRealationship />}
+        />
+        <Route path="/relationship" component={() => <RelationshipPage />} />
       </Switch>
     </Fragment>
   );
